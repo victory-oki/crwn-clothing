77dd28c3982a1ca0a98efc59e085e0c7fda9a490
victory-oki <victorysplogi@gmail.com>
2020-08-04 07:40:23 +0100

    finish signin handle submit method

--- src/App.js
+++ src/App.js
@@ -27,15 +27,12 @@ class App extends React.Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapshot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapshot.id,
-                ...snapshot.data(),
-              },
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
             },
-            () => console.log(this.state)
-          );
+          });
         });
       } else {
         this.setState({
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.util";
+import {
+  signInWithGoogle,
+  auth,
+  createUserProfileDocument,
+} from "../../firebase/firebase.util";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -13,9 +17,16 @@ class SignIn extends React.Component {
       password: "",
     };
   }
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
   handleChange = (event) => {
     const { value, name } = event.target;
